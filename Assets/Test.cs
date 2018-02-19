ee7f967c7dbab14f46a51903ac2ddd43be4d8c30
NaokiMizoe <orfv0x@ezweb.ne.jp>
2018-02-20 03:28:34 +0900

    GameSceneを追加

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class Test : MonoBehaviour {
+
+	// use this for initialization
+	void start () {
+		// 要素数５個の配列を初期化する
+		int[] array = { 50, 40, 29, 58, 49 };
+		// 配列の要素を順番に表示する
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (array [i]);
+		}
+	// 配列の要素を逆順に表示する
+		for (int p = 0; p >= 0; p--) {
+			Debug.Log (array [p]);
+		}
+	}
+
+
+
+
+			
+
+			// Update is called once per frame
+	void Update () {
+		
+	}
+}
